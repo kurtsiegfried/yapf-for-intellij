d0f68b6454cd75e810bfb6fd2abf9c66f656a1c8
GitHub Action <action@github.com>
2020-12-29 20:16:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/kurtsiegfried/yapfforintellij/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/kurtsiegfried/yapfforintellij/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kurtsiegfried.yapfforintellij.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kurtsiegfried.yapfforintellij.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/kurtsiegfried/yapfforintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kurtsiegfried.yapfforintellij.services
+
+import com.github.kurtsiegfried.yapfforintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
